e19c9a3010f316bc16c04c4012deb79699869b65
scrazzz <scruzism@mail.com>
2026-01-26 10:42:19 +0530

    Update utils

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,17 +1,6 @@
 import { APIApplicationCommandInteraction, APIMessageComponentInteraction, APIUser } from 'discord-api-types/v10'
 import { DISCORD_BASE_API } from './consts'
 
-export async function deleteMessage(botToken: string, channelId: string, messageId: string) {
-    const resp = await fetch(`${DISCORD_BASE_API}/channels/${channelId}/messages/${messageId}`, {
-        method: 'DELETE',
-        headers: {
-            'User-Agent': `BookmarkBot (https://github.com/scrazzz/bookmark-bot)`,
-            Authorization: `Bot ${botToken}`,
-        },
-    })
-    return resp.ok
-}
-
 export function createJumpUrl(guildId: string, channelId: string, messageId: string) {
     return `https://discord.com/channels/${guildId}/${channelId}/${messageId}`
 }
@@ -21,7 +10,6 @@ export function formatUsername(user: APIUser, mention: boolean = false) {
         return `<@${user.id}>`
     }
     if (user.bot === true) {
-        // Bot's don't have @username
         return `${user.username}#${user.discriminator}`
     }
     return `@${user.username}`
