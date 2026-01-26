6ddfc58946ba407e09ae6147303a3f44f0da638f
scrazzz <scruzism@mail.com>
2026-01-26 16:33:16 +0530

    Fix aboutme command

--- src/responses/aboutme.ts
+++ src/responses/aboutme.ts
@@ -1,4 +1,4 @@
-import { ButtonStyle, ComponentType, InteractionResponseType } from 'discord-api-types/v10'
+import { ButtonStyle, ComponentType, InteractionResponseType, MessageFlags } from 'discord-api-types/v10'
 import { Context } from 'hono'
 import { BOT_GITHUB_URL, BOT_INSTALL_URL } from '../utils/consts'
 
@@ -6,14 +6,18 @@ export async function aboutmeHandler(c: Context) {
     return c.json({
         type: InteractionResponseType.ChannelMessageWithSource,
         data: {
+            flags: MessageFlags.IsComponentsV2,
             components: [
                 {
                     type: ComponentType.Container,
                     components: [
                         {
-                            type: ComponentType.TextInput,
+                            type: ComponentType.TextDisplay,
                             content:
                                 'Easily bookmark any message from any server right to your DMs or to a specific channel via webhook.',
+                        },
+                        {
+                            type: ComponentType.Separator,
                         },
                         {
                             type: ComponentType.ActionRow,
@@ -21,19 +25,20 @@ export async function aboutmeHandler(c: Context) {
                                 {
                                     type: ComponentType.Button,
                                     style: ButtonStyle.Link,
-                                    label: 'Add Bot',
+                                    label: 'Add bot',
                                     url: BOT_INSTALL_URL,
                                 },
                                 {
                                     type: ComponentType.Button,
                                     style: ButtonStyle.Link,
-                                    label: 'Source code',
+                                    label: 'Source',
                                     url: BOT_GITHUB_URL,
                                 },
                                 {
                                     type: ComponentType.Button,
                                     style: ButtonStyle.Secondary,
                                     label: 'Developed by @scruzism',
+                                    custom_id: 'null',
                                     disabled: true,
                                 },
                             ],
